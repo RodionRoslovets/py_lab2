0150d8eef5d82395851d1883d07c0b3bf139f137
Rodion <rodion18570@gmail.com>
2023-03-08 14:27:21 +0300

    task 2

--- main.py
+++ main.py
@@ -11,4 +11,21 @@ def task1():
     
     print(" ".join(words))
 
+def task2():
+    isBreak = False
+
+    while isBreak != True:
+        word = input('Введите слово: ')
+        separatedWord = list(word.lower())
+        hasFs = 'ф' in separatedWord
+
+        if(word == 'stop'):
+            isBreak = True
+        else:
+            if hasFs:
+                print("Редкое слово")
+            else:
+                print("Не очень редкое слово")
+
 task1()
+task2()
